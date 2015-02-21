bd80ffa95a661e83a31df014b1a7e61bd0e8e947
U-TANDEM\ckingsley <kingsley.chris@gmail.com>
2015-02-20 17:27:26 -0800

    Changes to exercise 1

--- 1_locating_mutations/findMutations.py
+++ 1_locating_mutations/findMutations.py
@@ -2,3 +2,12 @@
 
 import os, re, sys
 
+# construct the trie data structure for the passed set of patterns
+def trieConstruction(patterns):
+    trie = dict()
+    
+    for pattern in patterns:
+        print pattern
+        
+        
+trieConstruction(['ATAGA','ATC','GAT'])
