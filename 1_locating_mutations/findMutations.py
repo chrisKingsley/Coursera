50bce385b8baf279254a1c940795dcc217aee7de
chrisKingsley <kingsley.chris@gmail.com>
2015-03-17 00:40:30 -0700

    additions to week1

--- 1_locating_mutations/findMutations.py
+++ 1_locating_mutations/findMutations.py
@@ -7,7 +7,26 @@ def trieConstruction(patterns):
     trie = dict()
     
     for pattern in patterns:
-        print pattern
+        currentNode = trie
         
+        for i in range(len(pattern)):
+            if pattern[i] in currentNode:
+                currentNode = currentNode[ pattern[i] ]
+            else:
+                newNode = dict()
+                currentNode[ pattern[i] ] = newNode
+                currentNode = newNode
+                
+    return trie
+    
+    
+def printTrieAdjacency(trie, num=0, total=0):
+    for node in sorted(trie):
+        print '%d->%d:%s' % (num, total+1, node)
+        total = printTrieAdjacency(trie[node], num+1, total+1)
         
-trieConstruction(['ATAGA','ATC','GAT'])
+    return total
+        
+trie = trieConstruction(['ATAGA','ATC','GAT'])
+printTrieAdjacency(trie)
+
