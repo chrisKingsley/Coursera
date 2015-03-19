9b11eda76c06a1ec80be03a1618b68468ace0089
chrisKingsley <kingsley.chris@gmail.com>
2015-03-18 23:11:04 -0700

    updates to week1

--- 1_locating_mutations/findMutations.py
+++ 1_locating_mutations/findMutations.py
@@ -23,10 +23,50 @@ def trieConstruction(patterns):
 def printTrieAdjacency(trie, num=0, total=0):
     for node in sorted(trie):
         print '%d->%d:%s' % (num, total+1, node)
-        total = printTrieAdjacency(trie[node], num+1, total+1)
+        total = printTrieAdjacency(trie[node], max(total+1, num+1), total+1)
         
     return total
         
-trie = trieConstruction(['ATAGA','ATC','GAT'])
-printTrieAdjacency(trie)
+# trie = trieConstruction(['ATAGA','ATC','GAT'])
+# printTrieAdjacency(trie)
 
+
+def prefixTrieMatching(text, trie):
+    pos = 0
+    v = trie
+    
+    while pos < len(text):
+        if len(v)==0:
+            return text[:pos]
+        elif text[pos] in v:
+            #print text[pos], pos, len(text), text
+            v = v[ text[pos] ]
+            pos += 1
+            # if pos==len(text):
+                # return text
+        else:
+            #print 'no matches found'
+            return None
+            
+def trieMatching(text, trie):
+    positions = []
+    for i in range(len(text)):
+        if prefixTrieMatching(text[i:], trie) is not None:
+            positions.append(i)
+            
+    return positions
+
+# trie = trieConstruction(['ATCG','GGGT'])
+# print trieMatching('AATCGGGTTCAATCGGGGT', trie)
+
+
+suffixTrieConstruction(seq):
+    trie = dict()
+    for i in range(len(seq)):
+        currentNode = trie
+        for j in range(i, len(seq)):
+            if seq[j] in currentNode:
+                currentNode = currentNode[ seq[j] ]
+            else
+                newNode = dict()
+                currentNode[ seq[j] ] = (seq[j], j)
