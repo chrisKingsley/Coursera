a045cdf22ea7ef1db1d2c1b83b1bf9ca4f08fedf
chrisKingsley <kingsley.chris@gmail.com>
2015-03-30 00:28:10 -0700

    lesson 1 updates

--- 1_locating_mutations/findMutations.py
+++ 1_locating_mutations/findMutations.py
@@ -63,28 +63,6 @@ def trieMatching(text, trie):
 # print trieMatching('AATCGGGTTCAATCGGGGT', trie)
 
 
-# class Edge:
-    # def __init__(self, symbol, pos):
-        # self.symbol = symbol
-        # self.pos = pos
-    # def __hash__(self):
-        # return hash(self.symbol)
-    # def __eq__(self, other):
-        # return self.symbol==other.symbol and self.pos==other.pos
-    # def updatePos(self, pos):
-        # if pos < self.pos:
-            # self.pos=pos
-    
-    
-    
-    
-# def nodeContainsChar(currentNode, char):
-    # for charTuple in currentNode:
-        # if charTuple[0]==char:
-            # return True
-    # return False
-    
-    
 # Construct a suffix trie
 def modifiedSuffixTrieConstruction(seq):
     trie = dict()
@@ -116,19 +94,91 @@ def getTrieEdges(trie, edge='', edges=[]):
         
     return edges
 
-
-def getLongestRepeat(seq):
-    trie = modifiedSuffixTrieConstruction(seq)
-    edges = getTrieEdges(trie)
-    
-    print Counter(edges)
-    
-    
-    
 # trie = modifiedSuffixTrieConstruction('ATAAATG$')
 # print getTrieEdges(trie)
+    
+    
+def getLongestRepeat(trie, edge='', longRep='', depth=0):
+    while len(trie)==1:
+        char, node = trie.popitem()
+        edge += char
+        if isinstance(node[1], dict):
+            trie = node[1]
+    
+    if len(trie) > 1:
+        if len(edge) > len(longRep):
+            longRep = edge
+        for char, node in trie.iteritems():
+            if isinstance(node[1], dict):
+                longRep = getLongestRepeat(node[1], edge+char, longRep, depth+1)
 
-getLongestRepeat('ATATCGTTTTATCGTT')
+    return longRep
 
-# tree = modifiedSuffixTreeConstruction(trie)
-# print tree
+# trie = modifiedSuffixTrieConstruction('ATATCGTTTTATCGTT$')
+# print trie
+# print getLongestRepeat(trie)  # should be TATCGTT
+
+
+def seqInTrie(trie, seq):
+    #print seq
+    for i in range(len(seq)):
+        
+        if isinstance(trie, dict) and seq[i] in trie:
+            node = trie[ seq[i] ]
+            trie = node[1]
+            #print seq[i], "True"
+        else:
+            #print seq[i], "False"
+            return False
+    return True
+    
+    
+def getLongestSharedSubstring(trie, seq):
+    longSeq = ''
+    
+    for i in range(len(seq)):
+        for j in range(i+1, len(seq)+1):
+            if (j-i) > len(longSeq) and seqInTrie(trie, seq[i:j]):
+                longSeq = seq[i:j]
+                print (j-i), seq[i:j]
+                
+    return longSeq
+    
+# seq1, seq2 = 'TCGGTAGATTGCGCCCACTC','AGGGGCTCGCAGTGTAAGAA'
+# trie = modifiedSuffixTrieConstruction(seq2)
+# print trie.keys()
+# print getLongestSharedSubstring(trie, seq1)
+    
+    
+def getShortestUnsharedSubstring(trie, seq):
+    shortSeq = seq
+    
+    for i in range(len(seq)):
+        for j in range(i+1, len(seq)+1):
+            if (j-i) < len(shortSeq) and not seqInTrie(trie, seq[i:j]):
+                shortSeq = seq[i:j]
+                print (j-i), seq[i:j]
+                
+    return shortSeq
+    
+# seq1, seq2 = 'CCAAGCTGCTAGAGG', 'CATGCTGGGCTGGCT'
+# trie = modifiedSuffixTrieConstruction(seq2)
+# print trie.keys()
+# print getShortestUnsharedSubstring(trie, seq1) # should be CC
+
+
+def getSuffixArray(seq):
+    suffixArray = []
+    for i in range(len(seq)):
+        suffixArray.append(seq[i:])
+    suffixArray.sort()
+    
+    for i in range(len(suffixArray)):
+        suffixArray[i] = len(seq)-len(suffixArray[i])
+        
+    return suffixArray
+    
+# suffixArray = getSuffixArray('AACGATAGCGGTAGA$')
+# print ', '.join([ str(x) for x in suffixArray ])
+
+
