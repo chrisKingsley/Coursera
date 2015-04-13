426025c8fc5f4f4a6cefcacb758ef5af6ae1f7ef
U-TANDEM\ckingsley <kingsley.chris@gmail.com>
2015-04-13 15:13:45 -0700

    updates to lesson 2

--- 2_evolutionary_trees/sars.py
+++ 2_evolutionary_trees/sars.py
@@ -2,3 +2,44 @@
 
 import math, os, random, re, sys
 
+
+
+def readAdjList(infile):
+    adjDict = dict()
+    
+    infile = open(infile, 'r')
+    numLeaves = int(infile.readline())
+    for line in infile:
+        if line.startswith('#') or len(line.rstrip())==0:
+            continue
+        start, stop, weight = re.split('->|:', line.rstrip())
+        adjDict[start] = adjDict.get(start, dict())
+        adjDict[start][stop] = int(weight)
+    infile.close()
+    
+    return adjDict, numLeaves
+    
+
+def getDist(adjDict, i, j, cumDist=0):
+    if i==j: return 0
+    
+    # if i in adjDict and j in adjDict[i]:
+        # dist = 
+    
+    return cumDist
+
+
+def getDistMatrix(adjDict, numLeaves):
+    distMat = [[0]*numLeaves for x in range(numLeaves)]
+    
+    for i in range(numLeaves):
+        for j in range(numLeaves):
+            distMat[i][j] = getDist(adjDict, i, j)
+    
+    return distMat
+
+adjDict, numLeaves = readAdjList('adjToDistMat.txt')
+print adjDict
+distMat = getDistMatrix(adjDict, numLeaves)
+print distMat
+
