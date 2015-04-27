fbdaf5acabc1e23c1722f9f1d021dfb59086d74c
chrisKingsley <kingsley.chris@gmail.com>
2015-04-26 20:58:34 -0700

    updates to lesson2

--- 2_evolutionary_trees/sars.py
+++ 2_evolutionary_trees/sars.py
@@ -192,39 +192,144 @@ def additivePhylogeny(distMat, n, tree=dict(), numNodes=None):
     # print '%s:%s' % (re.sub(':','->', key), tree[key])
 
 
-def getClusterDist(distMat, nodes1, nodes2):
-    dist = 0
+# returns the distance between two clusters of leaves
+# from the passed distance matrix
+def getClusterDist(distMat, leaves1, leaves2):
+    dist = 0.0
     
-    for node1 in nodes1:
-        for node2 in nodes2:
-            dist += distMat[node1][node2]
+    for leaf1 in leaves1:
+        for leaf2 in leaves2:
+            dist += distMat[leaf1][leaf2]
             
-    return dist/(len(nodes1)*len(nodes2))
+    return dist/(len(leaves1)*len(leaves2))
     
+# returns the cluster ID's and distance between the two
+# closest clusters of leaves
 def closestClusters(distMat, clusters):
-    minDist = sys.maxint
+    clusterId1, clusterId2, minDist = 0, 0, sys.maxint
+    clusterIds = clusters.keys()
     
-    for i in range(len(clusters)-1):
-        nodes1 = clusters[i]
-        for j in range(i+1, len(clusters)):
-            nodes2 = clusters[j]
-            dist = getClusterDist(distMat, nodes1, nodes2)
+    for i in range(len(clusterIds)-1):
+        leaves1 = clusters[ clusterIds[i] ]
+        for j in range(i+1, len(clusterIds)):
+            leaves2 = clusters[ clusterIds[j] ]
+            dist = getClusterDist(distMat, leaves1, leaves2)
             if dist < minDist:
                 minDist = dist
-                cluster1 = nodes1
-                cluster2 = nodes2
+                clusterId1 = clusterIds[i]
+                clusterId2 = clusterIds[j]
                 
-    return cluster1, cluster2, minDist
-
-
-def upgma(distMat, n):
-    tree = dict()
-    clusters = [ {x} for x in range(n) ]
+    return clusterId1, clusterId2, minDist
     
-    cluster1, cluster2, minDist = closestClusters(distMat, clusters)
-    print cluster1, cluster2, minDist
+
+# merges the two closest clusters of leaves, and updates ages
+# of internal nodes
+def mergeClusters(tree, distMat, clusters, ages):
+    i, j, minDist = closestClusters(distMat, clusters)
+    newCluster = clusters[i] | clusters[j]
+    clusterNum = max(clusters.keys()) + 1
+    
+    clusters[ clusterNum ] = newCluster
+    del clusters[ i ]
+    del clusters[ j ]
+    
+    tree[ getKey(i,clusterNum) ] = minDist/2.0 - ages.get(i, 0)
+    tree[ getKey(clusterNum,i) ] = tree[ getKey(i,clusterNum) ]
+    tree[ getKey(j,clusterNum) ] = minDist/2.0 - ages.get(j, 0)
+    tree[ getKey(clusterNum,j) ] = tree[ getKey(j,clusterNum) ]
+    
+    ages[ clusterNum ] = tree[ getKey(i,clusterNum) ] + ages.get(i, 0)
+
+
+# Implements UPGMA (Unweighted Pair Group Method with Arithmetic Mean)
+# for the passed distance matrix
+def upgma(distMat):
+    tree, ages = dict(), dict()
+    clusters = { x:{x} for x in range(len(distMat)) } # dict of dicts of leaves
+    
+    while len(clusters) > 1:
+        mergeClusters(tree, distMat, clusters, ages)
+    
+    return tree
      
-distMat = readDistMatFile('distMat2.txt', False)
+# distMat = readDistMatFile('distMat2.txt', False)
+# tree = upgma(distMat)
+# for key in sorted(tree):
+    # print '%s:%s' % (re.sub(':','->', key), tree[key])
 
-upgma(distMat, len(distMat))
 
+# uses neighbor joining to identify row/col indices
+# of two neighboring leaves
+def getNeighbors(distMat):
+    id1, id2, minDist = 0, 0, sys.maxint
+    totalDist = [ sum(distMat[i]) for i in range(len(distMat)) ]
+    
+    for i in range(len(distMat)-1):
+        for j in range(i+1, len(distMat)):
+            dist = (len(distMat)-2) * distMat[i][j] - \
+                    totalDist[i] - totalDist[j]
+            if dist < minDist:
+                id1, id2, minDist = i, j, dist
+    
+    delta = (totalDist[id1] - totalDist[id2])/(len(distMat)-2.0)
+    
+    return id1, id2, delta
+    
+    
+# takes the two neighboring leaves identified in the distance matrix
+# and merges them into one new node, thereby reducing the dimension
+# of the distance matrix
+def reduceMatrix(distMat, labels, i, j):
+    idx = [ x for x in range(len(distMat)) if x!=i and x!=j ]
+    
+    # create new label for merged node
+    newLabel = max(labels) + 1
+    newLabels = [ labels[x] for x in idx ]
+    newLabels.append(newLabel)
+    
+    # allocate new distance matrix and copy values
+    newMat = [[0]*len(newLabels) for x in range(len(newLabels)) ]
+    for k in range(len(idx)):
+        for l in range(len(idx)):
+            newMat[k][l] = distMat[ idx[k] ][ idx[l] ]
+            
+    # calculate distance from new nodes to old nodes
+    m = len(newMat)-1
+    for k in range(len(idx)):
+        newMat[m][k] = 0.5*(distMat[idx[k]][i] + distMat[idx[k]][j] - \
+                            distMat[i][j])
+        newMat[k][m] = newMat[m][k]
+    
+    return newMat, newLabels
+   
+
+# adds edges to the new node in the tree, connecting to existing nodes   
+def addEdges(tree, distMat, labels, newLabel, i, j, delta):
+    m = len(distMat)-1
+    
+    tree[ getKey(labels[i],newLabel) ] = (distMat[i][j] + delta)/2.0
+    tree[ getKey(newLabel,labels[i]) ] = tree[ getKey(labels[i],newLabel) ]
+    tree[ getKey(labels[j],newLabel) ] = (distMat[i][j] - delta)/2.0
+    tree[ getKey(newLabel,labels[j]) ] = tree[ getKey(labels[j],newLabel) ]
+
+    
+# use the neighbor joining algorithm to construct a tree based on the
+# passed distance matrix
+def neighborJoining(distMat, labels, tree=dict()):
+    if len(distMat)==2:
+        tree[ getKey(labels[0],labels[1]) ] = distMat[0][1]
+        tree[ getKey(labels[1],labels[0]) ] = distMat[1][0]
+        return tree
+        
+    i, j, delta = getNeighbors(distMat)
+    newMat, newLabels = reduceMatrix(distMat, labels, i, j)
+    
+    tree = neighborJoining(newMat, newLabels, tree)
+    addEdges(tree, distMat, labels, newLabels[-1], i, j, delta)
+    
+    return tree
+    
+distMat = readDistMatFile('distMat3.txt', False)
+tree = neighborJoining(distMat, range(len(distMat)))
+for key in sorted(tree):
+    print '%s:%s' % (re.sub(':','->', key), tree[key])
