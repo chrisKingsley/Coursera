82896b074558be1fee94757874ecf3f7b60b01fb
U-TANDEM\ckingsley <kingsley.chris@gmail.com>
2015-05-27 17:25:03 -0700

    updates to lesson4

--- 4_HMM/hiv.py
+++ 4_HMM/hiv.py
@@ -1,51 +1,131 @@
 #!/usr/bin/env python
 
-import os, re, sys
+import math, os, re, sys
 
-def readTransitionFile(filePath, emissionProbs=False):
-    infile = open(filePath, 'r')
+
+def readProbTable(contents, lineNum):
+    probMat = dict()
     
-    if emissionProbs:
-        emittedSeq = infile.readline().rstrip()
-        infile.readline();infile.readline();infile.readline()
-        
-    hiddenSeq = infile.readline().rstrip()
-    infile.readline();infile.readline();infile.readline()
+    states1 = contents[lineNum].split()
+    lineNum +=1
     
-    transMat = dict()
-    states1 = infile.readline().rstrip().split()
-    for line in infile:
-        tokens = line.rstrip().split()
+    while lineNum < len(contents):
+        if re.match('-+', contents[lineNum]):
+            break
+        tokens = contents[lineNum].split()
         state2 = tokens[0]
-        for i in range(1, len(tokens)):
-            key = state2 + states1[i-1]
-            transMat[ key ] = float(tokens[i])
+        probMat[ state2 ] = { states1[i-1]:float(tokens[i]) for i in range(1, len(tokens)) }
+        lineNum += 1
+            
+    return probMat, lineNum + 1
+        
+        
+def readTransitionFile(filePath, readEmittedSeq=True, readHiddenPath=False,
+                       readTransitionProbs=True, readEmissionProbs=True):
+    lineNum = 0
+    infile = open(filePath, 'r')
+    contents = [ x.rstrip() for x in infile.readlines() ]
     infile.close()
     
-    if emissionProbs:
-        return emittedSeq, hiddenSeq, transMat
+    if readEmittedSeq:
+        emittedSeq = contents[lineNum]
+        lineNum += 4
+    
+    if readHiddenPath:
+        hiddenPath = contents[lineNum]
+        lineNum += 4
     else:
-        return hiddenSeq, transMat
+        lineNum +=2
     
-def calcTransProb(hiddenSeq, transMat):
+    if readTransitionProbs:
+        transMat, lineNum = readProbTable(contents, lineNum)
+    
+    if readEmissionProbs:
+        emissionMat, lineNum = readProbTable(contents, lineNum)
+    
+    retVal = []
+    if readEmittedSeq: retVal.append( emittedSeq )
+    if readHiddenPath: retVal.append( hiddenPath )
+    if readTransitionProbs: retVal.append( transMat )
+    if readEmissionProbs: retVal.append( emissionMat )
+    return retVal
+    
+    
+def calcTransProb(hiddenPath, transMat):
     prob = 0.5
-    for i in range(len(hiddenSeq)-1):
-        prob *= transMat[ hiddenSeq[i:i+2] ]
+    for i in range(len(hiddenPath)-1):
+        prob *= transMat[ hiddenPath[i] ][ hiddenPath[i+1] ]
         
     return prob
     
-# hiddenSeq, transMat = readTransitionFile('transitionProbs.txt')
-# print calcTransProb(hiddenSeq, transMat)
+# hiddenPath, transMat = readTransitionFile('transitionProbs.txt', False, True, True, False)
+# print calcTransProb(hiddenPath, transMat)
 
 
-def calcEmissionProb(emittedSeq, hiddenSeq, emissionMat):
+def calcEmissionProb(emittedSeq, hiddenPath, emissionMat):
     prob = 1.0
-    for i in range(len(hiddenSeq)):
-        prob *= emissionMat[ hiddenSeq[i] + emittedSeq[i] ]
+    for i in range(len(hiddenPath)):
+        prob *= emissionMat[ hiddenPath[i] ][ emittedSeq[i] ]
         
     return prob
     
-# emittedSeq, hiddenSeq, emissionMat = \
-    # readTransitionFile('transitionProbs2.txt', emissionProbs=True)
-# print calcEmissionProb(emittedSeq, hiddenSeq, emissionMat)
+# emittedSeq, hiddenPath, emissionMat = \
+    # readTransitionFile('transitionProbs2.txt', True, True, False, True)
+# print calcEmissionProb(emittedSeq, hiddenPath, emissionMat)
+
+
+def getScoringMat(emittedSeq, emissionMat, transitionMat):
+    hiddenStates = sorted(transitionMat.keys())
+    stateProbs = [ [0]*len(hiddenStates) for x in range(len(emittedSeq)) ]
+    
+    # initialize first entry in state probability matrix
+    stateProbs[0] = [1.0/len(hiddenStates)] * len(hiddenStates)
+    for i in range(len(hiddenStates)):
+        stateProbs[0][i] *= emissionMat[ hiddenStates[i] ][ emittedSeq[0] ]
+    
+    # complete the scoring matrix
+    for i in range(1, len(emittedSeq)):
+        for j in range(len(hiddenStates)):
+            probs = []
+            for k in range(len(hiddenStates)):
+                prob = stateProbs[i-1][k] * \
+                       transitionMat[ hiddenStates[k] ][ hiddenStates[j] ] *\
+                       emissionMat[ hiddenStates[j] ][ emittedSeq[i] ]
+                probs.append( prob )
+            stateProbs[i][j] = max(probs)
+            
+    return stateProbs
+    
+    
+def getHiddenPath(stateProbs, emissionMat, transitionMat, emittedSeq):
+    hiddenStates = sorted(transitionMat.keys())
+    
+    # identify last state in the sequence of hidden states
+    maxProbIdx = stateProbs[-1].index(max(stateProbs[-1]))
+    hiddenPath = hiddenStates[ maxProbIdx ]
+    
+    # identify successive hidden states
+    for i in range(len(stateProbs)-2, -1, -1):
+        probs = []
+        for j in range(len(hiddenStates)):
+            prob = stateProbs[i][j] * \
+                   transitionMat[ hiddenStates[j] ][ hiddenPath[0] ] * \
+                   emissionMat[ hiddenPath[0] ][ emittedSeq[i+1] ]
+            probs.append( prob )
+        maxProbIdx = probs.index(max(probs))
+        hiddenPath = hiddenStates[maxProbIdx] + hiddenPath
+    
+    return hiddenPath
+
+def findOptimalHiddenPath(emittedSeq, emissionMat, transitionMat):
+    stateProbs = getScoringMat(emittedSeq, emissionMat, transitionMat)
+    hiddenPath = getHiddenPath(stateProbs, emissionMat, transitionMat, emittedSeq)
+    
+    return hiddenPath
+    
+    
+emittedSeq, transitionMat, emissionMat = \
+    readTransitionFile('transitionProbs3.txt', readEmissionProbs=True)
+print findOptimalHiddenPath(emittedSeq, emissionMat, transitionMat)
+
 
